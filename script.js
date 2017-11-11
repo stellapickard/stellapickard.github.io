614a102602eae4ef91fdb2dc16dfce534cead41b
stellapickard <stella@pickard.co.za>
2017-11-11 15:50:39 +0200

    Some brushing up

--- script.js
+++ script.js
@@ -6,22 +6,10 @@ $(document).ready(function(){
 			console.log('yo');
 	});
 
-	$('.cards').mouseout(function(){
-		$(this).find('.hover_box').css('display','none');
-		$(this).find('.base_layer').css('display', 'block');
-	});
-
-
-	// $('.cards').onclick(function(){
-	// 	if ($(this).('.base_layer') = 'block'){
-	// 		$.(this).('base_layer').css('display', 'none');
-	// 		$.(this).('hover_box').css('display', 'block');
-	// 	} else if ($(this).('base_layer') = 'none'){
-	// 		$.(this).('.base_layer').css('display', 'block');
-	// 		$.(this).('.hover_box').css('display', 'none');
-	// 		};
-	// 	};
-
+	// $('.cards').mouseout(function(){
+	// 	$(this).find('.hover_box').css('display','none');
+	// 	$(this).find('.base_layer').css('display', 'block');
+	// });
 
 });
 
