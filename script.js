7e7878d67ce5a9c54a94bea24b36e1c1bb5ee976
stellapickard <stella@pickard.co.za>
2017-11-11 19:57:00 +0200

    Some brushing up

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ $(document).ready(function(){
 	$('.cards').mouseover(function(){
 		$(this).find('.hover_box').css('display','block');
 		$(this).find('.base_layer').css('display', 'none');
-			console.log('yo');
-			sleep(10);
 	});
 
 	$('.cards').mouseout(function(){
 		$(this).find('.hover_box').css('display','none');
 		$(this).find('.base_layer').css('display', 'block');
-		sleep(10);
 	});
-	function sleep(delay) {
-        var start = new Date().getTime();
-        while (new Date().getTime() < start + delay);
-      }
-
 });
 
 
